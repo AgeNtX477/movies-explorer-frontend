708da52d246658707e4b5f8498d91183ea527ae0
Jury Obuhov <jury2500@gmail.com>
2023-01-20 03:25:24 +0300

    1280 desktop done

--- src/components/MoviesCardList/MoviesCardList.jsx
+++ src/components/MoviesCardList/MoviesCardList.jsx
@@ -1,7 +1,35 @@
 import './MoviesCardList.css'
+import MovieCard from '../MoviesCard/MoviesCard'
+import { Route, Switch } from 'react-router-dom'
 
 function MoviesCardList () {
-  return <div>MoviesCardListMoviesCardListMoviesCardList</div>
+  return (
+    <Switch>
+      <Route exact path='/movies'>
+        <section className='MoviesCardList'>
+          <div className='MoviesCardList__cardlist-container'>
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+            <MovieCard />
+          </div>
+          <button className='MovesCardList__more-button'>Еще</button>
+        </section>
+      </Route>
+      <Route exact path='/saved-movies'>
+        <section className='MoviesCardList'>
+          <div className='MoviesCardList__cardlist-container MoviesCardList__cardlist-container_saved'>
+            <MovieCard />
+            <MovieCard /> 
+            <MovieCard />
+            <MovieCard />
+          </div>
+        </section>
+      </Route>
+    </Switch>
+  )
 }
 
 export default MoviesCardList
